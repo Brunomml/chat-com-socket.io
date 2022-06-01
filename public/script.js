87aa108cdb0938bc6f57b9bc9cc3151ae461b3c8
Brunomml <brunomyguel.miranda1988@gmail.com>
2022-05-31 22:15:16 -0300

    style

--- public/script.js
+++ public/script.js
@@ -3,12 +3,16 @@ const socket = io()
 socket.on("drawMensage", (text)=>{
     const div = document.querySelector(".mensage")
 
-    div.innerText += `
-        ${text}
-    `
+    div.innerText += `${text}`
+    div.innerHTML += "<br/>"
 })
 
 function sendMensage() {
-    const inputText = document.querySelector(".inputText")
+    const inputText = document.querySelector(".text")
     socket.emit("mensage", `${inputText.value}`)
 }
+addEventListener("keydown", (event)=>{
+    if (event.key == "Enter") {
+        sendMensage()
+    }
+})
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express()
 const http = require("http")
 const server = http.createServer(app)
 const socket = require("socket.io")(server)
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
+
 
 app.use(express.static("public"))
 
